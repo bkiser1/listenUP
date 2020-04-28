41a4d15cd0463649df19f4d2eed2792c554c3b39
brian K <briank@brians-MacBook-Air.local>
2020-04-28 19:20:51 -0400

    inital deploy

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//server static assets
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	//set static folder
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port${PORT}`));
