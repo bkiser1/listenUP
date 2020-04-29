ff09ee72904045db90970549078c9ec923e67d2f
brian K <briank@brians-MacBook-Air.local>
2020-04-29 17:46:07 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const corsOptions = {
+	origin: "https://kiser-new-app.herokuapp.com/",
+	optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.options("*", cors());
+app.use("/", routes);
 
 //server static assets
 if (process.env.NODE_ENV === "production") {
@@ -17,3 +26,11 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port${PORT}`));
+
+const corsOptions = {
+	origin: "https://your-app-name.herokuapp.com",
+	optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.options("*", cors());
+app.use("/", routes);
